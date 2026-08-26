ac450fbe41173877318aa6b3314fb53030179e63
@algoreadme <dhikaaditya1@gmail.com>
2026-08-26 22:34:40 +0700

    refactor: prefix console output in test cases and remove obsolete playground metadata files

--- Pages/01-Two-Sum.xcplaygroundpage/Contents.swift
+++ Pages/01-Two-Sum.xcplaygroundpage/Contents.swift
@@ -3,10 +3,10 @@ import Foundation
 // MARK: - Test Cases
 let solution = TwoSumSolutions()
 let test1 = solution.twoSum([2, 7, 11, 15], 9)
-print("Test Case 1: nums = [2,7,11,15], target = 9 -> Result: \(test1) (Expected: [0, 1])")
+print("1 Test Case 1: nums = [2,7,11,15], target = 9 -> Result: \(test1) (Expected: [0, 1])")
 
 let test2 = solution.twoSum([3, 2, 4], 6)
-print("Test Case 2: nums = [3,2,4], target = 6 -> Result: \(test2) (Expected: [1, 2])")
+print("1 Test Case 2: nums = [3,2,4], target = 6 -> Result: \(test2) (Expected: [1, 2])")
 
 let test3 = solution.twoSum([3, 3], 6)
-print("Test Case 3: nums = [3,3], target = 6 -> Result: \(test3) (Expected: [0, 1])")
+print("1 Test Case 3: nums = [3,3], target = 6 -> Result: \(test3) (Expected: [0, 1])")
--- Pages/02-Contains-Duplicate.xcplaygroundpage/Contents.swift
+++ Pages/02-Contains-Duplicate.xcplaygroundpage/Contents.swift
@@ -16,10 +16,10 @@ class Solution {
 // MARK: - Test Cases
 let solution = Solution()
 let test1 = solution.containsDuplicate([1, 2, 3, 1])
-print("Test Case 1: nums = [1,2,3,1] -> Result: \(test1) (Expected: true)")
+print("2 Test Case 1: nums = [1,2,3,1] -> Result: \(test1) (Expected: true)")
 
 let test2 = solution.containsDuplicate([1, 2, 3, 4])
-print("Test Case 2: nums = [1,2,3,4] -> Result: \(test2) (Expected: false)")
+print("2 Test Case 2: nums = [1,2,3,4] -> Result: \(test2) (Expected: false)")
 
 let test3 = solution.containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])
-print("Test Case 3: nums = [1,1,1,3,3,4,3,2,4,2] -> Result: \(test3) (Expected: true)")
+print("2 Test Case 3: nums = [1,1,1,3,3,4,3,2,4,2] -> Result: \(test3) (Expected: true)")
